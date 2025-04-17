6748a7dbabaab4585d4078b19ae12fe421cc2e04
rajeev <rajeevkumar25112002@gmail.com>
2025-04-17 22:10:04 +0530

    Add dbConnect function and update package.json with new dependencies

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+
+async function dbConnect():Promise<void>  {
+    if(connection.isConnected){
+        console.log("db connection already established")
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '')
+        console.log("connection:",db);
+        connection.isConnected= db.connections[0].readyState
+        
+        
+    } catch (error) {
+        console.log("db connection failed")
+        process.exit(1)
+
+        
+    }
+    
+}
